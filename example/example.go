6906a87c87e47194c7f272a850a2b40cb575762f
bamchoh <bamchoh@gmail.com>
2017-07-04 21:18:43 +0900

    Refactor API

--- example/example.go
+++ example/example.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"io"
 	"log"
-	"os"
+	"sync"
 
-	"github.com/bamchoh/pollydent"
+	polly "github.com/bamchoh/pollydent"
 )
 
 func main() {
-	f, _ := os.Create("pollydent.log")
-	logger := log.New(f, "pollydent:", 0)
-	p, _ := pollydent.NewPolly(logger, "pollydent.yml")
-	p.ReadAloud("こんにちは世界")
+	config, err := polly.Load("pollydent.yml")
+	if err != nil {
+		panic(err)
+	}
+	p := polly.NewPollydent(
+		"<ACCESS_KEY>",
+		"<SECRET_KEY>",
+		config,
+	)
+
+	// Example 1
+	// Read Aloud Example
+	err = p.ReadAloud("こんにちは世界")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Example 2
+	// SendToPolly / Play Example
+	params := []polly.SpeechParams{
+		polly.SpeechParams{"こんばんわ世界", "Mizuki", 100},
+		polly.SpeechParams{"おはようございます世界", "Mizuki", 200},
+		polly.SpeechParams{"Hello World", "Joey", 100},
+	}
+
+	var strm io.Reader
+	var wg sync.WaitGroup
+	for _, param := range params {
+		wg.Add(1)
+		strm, err = p.SendToPolly(param)
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func(strm2 io.Reader) {
+			p.Play(strm2)
+			wg.Done()
+		}(strm)
+	}
+
+	wg.Wait()
 }
--- polly.go
+++ polly.go
@@ -2,9 +2,6 @@ package pollydent
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 	"strconv"
 	"sync"
 	"time"
@@ -15,11 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/polly"
 	"github.com/hajimehoshi/oto"
 
-	"errors"
 	"io"
-	"io/ioutil"
-
-	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -28,47 +21,69 @@ var (
 	byteParSample = 2
 )
 
-// PollyConfig is configuration structure for Polly
-type PollyConfig struct {
-	AccessKey string `yaml:"access_key"`
-	SecretKey string `yaml:"secret_key"`
-	Region    string `yaml:"region"`
-	Format    string // Fixed pcm
-	Voice     string `yaml:"voice"`
-	TextType  string `yaml:"text_type"`
-	Polly     *polly.Polly
+type SpeechParams struct {
+	Message string
+	Voice   string
+	Speed   int
 }
 
-// Polly is structure to manage read aloud
-type Polly struct {
-	Logger    *log.Logger
-	Config    *PollyConfig
-	Counter   int
-	SendMutex *sync.Mutex
-	PlayMutex *sync.Mutex
+// Pollydent is structure to manage read aloud
+type Pollydent struct {
+	config    *PollyConfig
+	playMutex *sync.Mutex
+	sess      *session.Session
 }
 
-func (p *Polly) sendAws(msg string, speed int) (resp *polly.SynthesizeSpeechOutput, err error) {
-	p.SendMutex.Lock()
-	defer p.SendMutex.Unlock()
-	packedMsg := `<speak><prosody rate="` + strconv.Itoa(speed) + `%"><![CDATA[` + msg + `]]></prosody></speak>`
+// NewPollydent news Polly structure
+func NewPollydent(accessKey, secretKey string, config *PollyConfig) *Pollydent {
+	creds := credentials.NewStaticCredentials(accessKey, secretKey, "")
+	sess := session.New(&aws.Config{Credentials: creds})
 
-	resp, err = p.SynthesizeSpeech(packedMsg)
-	return
+	return &Pollydent{
+		config:    config,
+		playMutex: new(sync.Mutex),
+		sess:      sess,
+	}
+}
+func (p *Pollydent) SendToPolly(config SpeechParams) (io.Reader, error) {
+	var err error
+
+	if config.Speed == 0 {
+		config.Speed = p.config.Speed
+	}
+
+	if config.Voice == "" {
+		config.Voice = p.config.Voice
+	}
+
+	text := `<speak><prosody rate="` + strconv.Itoa(config.Speed) + `%"><![CDATA[` + config.Message + `]]></prosody></speak>`
+
+	pol := polly.New(p.sess, aws.NewConfig().WithRegion(p.config.Region))
+
+	params := &polly.SynthesizeSpeechInput{
+		OutputFormat: aws.String(p.config.format),
+		Text:         aws.String(text),
+		TextType:     aws.String(p.config.textType),
+		VoiceId:      aws.String(config.Voice),
+	}
+
+	resp, err := pol.SynthesizeSpeech(params)
+	if err != nil {
+		return nil, err
+	}
+	return resp.AudioStream, nil
 }
 
-func (p *Polly) play(resp *polly.SynthesizeSpeechOutput) (err error) {
-	p.PlayMutex.Lock()
-	defer p.PlayMutex.Unlock()
+func (p *Pollydent) Play(reader io.Reader) (err error) {
+	p.playMutex.Lock()
+	defer p.playMutex.Unlock()
 
 	totalData := make([]byte, 0)
 	for {
 		var n int
 		data := make([]byte, 65535)
-		if n, err = resp.AudioStream.Read(data); err != nil {
+		if n, err = reader.Read(data); err != nil {
 			if err != io.EOF {
-				p.Logger.Println("resp.AudioStream")
-				p.Logger.Println(err)
 				return
 			}
 			totalData = append(totalData, data[:n]...)
@@ -80,8 +95,6 @@ func (p *Polly) play(resp *polly.SynthesizeSpeechOutput) (err error) {
 
 	player, err := oto.NewPlayer(sampleRate, numOfChanel, byteParSample, len(totalData))
 	if err != nil {
-		p.Logger.Println("oto.NewPlayer")
-		p.Logger.Println(err)
 		return
 	}
 	defer player.Close()
@@ -95,8 +108,6 @@ func (p *Polly) play(resp *polly.SynthesizeSpeechOutput) (err error) {
 	}()
 
 	if _, err = player.Write(totalData); err != nil {
-		p.Logger.Println("io.Copy")
-		p.Logger.Println(err)
 		return
 	}
 
@@ -106,125 +117,17 @@ func (p *Polly) play(resp *polly.SynthesizeSpeechOutput) (err error) {
 }
 
 // ReadAloud reads aloud msg by Polly
-func (p *Polly) ReadAloud(msg string) (err error) {
+func (p *Pollydent) ReadAloud(msg string) (err error) {
 	if msgLen := len([]rune(msg)); msgLen > 1500 {
 		errMsg := "Message size is %d. Please pass with the length of 1500 or less."
 		err = fmt.Errorf(errMsg, msgLen)
-		p.Logger.Println(err)
 		return err
 	}
-	p.Counter++
-	defer func() { p.Counter-- }()
 
-	if p.Counter > 5 {
-		p.Logger.Println("Skipped : ", msg)
-		return
-	}
-
-	speed := 100
-	speed += 20 * (p.Counter - 1)
-
-	resp, err := p.sendAws(msg, speed)
+	strm, err := p.SendToPolly(SpeechParams{Message: msg})
 	if err != nil {
-		p.Logger.Println(err)
 		return
 	}
-	p.play(resp)
+	p.Play(strm)
 	return
-}
-
-// NewPolly news Polly structure
-func NewPolly(logger *log.Logger, loadFile string) (*Polly, error) {
-	var err error
-
-	p := &Polly{
-		Logger:    logger,
-		SendMutex: new(sync.Mutex),
-		PlayMutex: new(sync.Mutex),
-	}
-
-	basedir := filepath.Dir(os.Args[0])
-	filepath := filepath.Join(basedir, loadFile)
-	f, err := os.Open(filepath)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	defer f.Close()
-
-	p.Config, err = p.load(f)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
-	return p, err
-}
-
-func (p *Polly) load(r io.Reader) (*PollyConfig, error) {
-	var data []byte
-	var err error
-	var pc PollyConfig
-
-	data, err = ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	err = yaml.Unmarshal(data, &pc)
-	if err != nil {
-		return nil, err
-	}
-
-	errTxt := ""
-	if pc.AccessKey == "" {
-		errTxt += "value of access_key does not set in setting file. "
-	}
-
-	if pc.SecretKey == "" {
-		errTxt += "value of secret_key does not set in setting file. "
-	}
-
-	if errTxt != "" {
-		return nil, errors.New(errTxt)
-	}
-
-	if pc.Region == "" {
-		pc.Region = "us-west-2"
-	}
-
-	pc.Format = "pcm"
-
-	if pc.Voice == "" {
-		pc.Voice = "Mizuki"
-	}
-
-	if pc.TextType == "" {
-		pc.TextType = "ssml"
-	}
-
-	pc.Polly, err = p.initPolly(&pc)
-	if err != nil {
-		return nil, err
-	}
-
-	return &pc, err
-}
-
-func (p *Polly) initPolly(pc *PollyConfig) (*polly.Polly, error) {
-	creds := credentials.NewStaticCredentials(pc.AccessKey, pc.SecretKey, "")
-	sess := session.New(&aws.Config{Credentials: creds})
-	return polly.New(sess, aws.NewConfig().WithRegion(pc.Region)), nil
-}
-
-// SynthesizeSpeech is call aws-polly SynthesizeSpeech
-func (p *Polly) SynthesizeSpeech(text string) (*polly.SynthesizeSpeechOutput, error) {
-	pc := p.Config
-	params := &polly.SynthesizeSpeechInput{
-		OutputFormat: aws.String(pc.Format),
-		Text:         aws.String(text),
-		TextType:     aws.String(pc.TextType),
-		VoiceId:      aws.String(pc.Voice),
-	}
-	return pc.Polly.SynthesizeSpeech(params)
 }
